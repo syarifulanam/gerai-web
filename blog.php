c033b5ffea6d773940342e518c4982bc7f6ddb40
syarifulanam <em.syariful.anam@gmail.com>
2025-04-03 16:33:57 +0700

    gerai web

---
+++ blog.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Gerai Pajak</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <?php include 'library-head.php'; ?>
+
+</head>
+
+<body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
+
+    <!-- ======= Header ======= -->
+    <?php include 'header.php'; ?>
+
+    <main id="main" class="mt-3">
+
+
+        <section>
+            <h1 class="text-center mt-3">Welcome to Blog</h1>
+
+            <div class="container mt-3">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <div class="row">
+                            <div class="col-lg-4 mb-3 mt-3">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="https://app.idstar.co.id/static/media/idstar-vision.b8f7b0f3.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-lg-2">
+                        <ul class="list-group">
+                            <li class="list-group-item active" aria-current="true">Categories</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                            <li class="list-group-item">A fourth item</li>
+                            <li class="list-group-item">And a fifth one</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+    </main>
+
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- <?php include 'footer.php'; ?> -->
+
+    <?php include 'library-foot.php'; ?>
+
+</body>
+
+</html>
